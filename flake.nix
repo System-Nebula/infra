3557130f5d2469a9cc05b9fe20efe2c725b4c70b
FKouhai <frandres00@gmail.com>
2026-01-03 22:34:11 +0100

    chore: first commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Dev shell with Go and OpenTofu";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.go
+              pkgs.opentofu
+            ];
+          };
+        }
+      );
+    };
+}
